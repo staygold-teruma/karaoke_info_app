0930587f908b2cdab16c369d582176ea3ac78747
西川輝馬 <staygoldteruma@MacBookPro.local>
2021-07-16 00:55:21 +0900

    DrinkPlanモデルの一覧表示機能と新規登録機能を実装

--- app/controllers/drink_plans_controller.rb
+++ app/controllers/drink_plans_controller.rb
@@ -1,22 +1,33 @@
 class DrinkPlansController < ApplicationController
   def index
+    @q = DrinkPlan.ransack(params[:q])
+    @drink_plans = @q.result(distinct: true).order(id: "ASC")
   end
 
   def new
+    @drink_plan = DrinkPlan.new
   end
 
   def create
+    DrinkPlan.create(drink_plan_params)
+    redirect_to drink_plans_path
   end
 
-  def show
+  def show; end
+
+  def edit; end
+
+  def update; end
+
+  def destroy; end
+
+  private
+
+  def set_main_plan
+    @drink_plan = DrinkPlan.find(params[:id])
   end
 
-  def edit
-  end
-
-  def update
-  end
-
-  def destroy
+  def drink_plan_params
+    params.require(:drink_plan).permit(:name, :note, :time_unit, :adult_fee, :student_fee, :senior_fee, :child_fee)
   end
 end
