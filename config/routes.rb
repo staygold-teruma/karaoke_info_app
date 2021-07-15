6283b06ebc2975e00f027b181c443da09546ef74
西川輝馬 <staygoldteruma@MacBookPro.local>
2021-07-15 22:17:02 +0900

    resourcesでDrinkPlanモデルのルーティングを設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,5 @@
 Rails.application.routes.draw do
-  get 'drink_plans/index'
-  get 'drink_plans/new'
-  get 'drink_plans/create'
-  get 'drink_plans/show'
-  get 'drink_plans/edit'
-  get 'drink_plans/update'
-  get 'drink_plans/destroy'
   root "main_plans#index"
   resources :main_plans
+  resources :drink_plans
 end
