4bde94851dbc7b8610120959442d2047d4eb1daa
西川輝馬 <staygoldteruma@MacBookPro.local>
2021-07-27 03:44:28 +0900

    fee_infoコントローラを作成

---
+++ app/controllers/fee_info_controller.rb
@@ -0,0 +1,22 @@
+class FeeInfoController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+
+  def crreate
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
+  get 'fee_info/index'
+  get 'fee_info/new'
+  get 'fee_info/crreate'
+  get 'fee_info/show'
+  get 'fee_info/edit'
+  get 'fee_info/update'
+  get 'fee_info/destroy'
   root "main_plans#index"
+  resources :fee_infos
   resources :main_plans
   resources :drink_plans
 end
