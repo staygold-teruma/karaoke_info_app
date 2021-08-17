37d901f935a08dbd2013245458cc9213f9bff0e2
西川輝馬 <staygoldteruma@MacBookPro.local>
2021-08-17 11:07:38 +0900

    ImageUploaderとshopsテーブルのimageカラムを連携

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -3,4 +3,5 @@ class Shop < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  mount_uploader :image, ImageUploader
 end
