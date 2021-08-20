c0c42a220eaee4478a5012d8307fd39febc9f463
西川輝馬 <staygoldteruma@MacBookPro.local>
2021-08-21 04:40:22 +0900

    InformationモデルをShopモデルと関連付け

--- app/models/information.rb
+++ app/models/information.rb
@@ -1,6 +1,8 @@
 class Information < ApplicationRecord
+  belongs_to :shop
   with_options presence: true do
     validates :title
     validates :content
   end
+  mount_uploader :image, ImageUploader
 end
