1b7eda1a7fe9a9cc52633c96ededeac542a671f4
西川輝馬 <staygoldteruma@MacBookPro.local>
2021-08-28 15:56:09 +0900

    Informationモデルを削除

--- app/models/information.rb
+++
@@ -1,8 +0,0 @@
-class Information < ApplicationRecord
-  belongs_to :shop
-  with_options presence: true do
-    validates :title
-    validates :content
-  end
-  mount_uploader :image, ImageUploader
-end
