969df749fa09d756ad820d5c3f543417fb4e54ee
西川輝馬 <staygoldteruma@MacBookPro.local>
2021-08-28 17:11:29 +0900

    topicsコントローラを作成し、各アクションを記述

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,22 +1,38 @@
 class TopicsController < ApplicationController
   def index
+    @topics = Topic.includes(:shop).order(:created_at)
   end
 
   def new
+    @topic = Topic.new
   end
 
   def create
+    current_shop.topic.create!(topic_params)
+    redirect_to topics_path
   end
 
-  def show
-  end
+  def show; end
 
-  def edit
-  end
+  def edit; end
 
   def update
+    @topic.update!(topic_params)
+    redirect_to @topic
   end
 
   def destroy
+    @topic.destroy!
+    redirect_to topics_path
+  end
+
+  private
+
+  def set_topic
+    @topic = current_shop.topics.find(params[:id])
+  end
+
+  def topic_params
+    params.require(:topic).permit(:title, :content, :image)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,9 @@
 Rails.application.routes.draw do
-  get 'topics/index'
-  get 'topics/new'
-  get 'topics/create'
-  get 'topics/show'
-  get 'topics/edit'
-  get 'topics/update'
-  get 'topics/destroy'
   root "fee_guides#new"
   devise_for :shops
   resources :shops, only: [:index, :show]
   resources :fee_guides
   resources :main_plans
   resources :drink_plans
+  resources :topics
 end
