a89543489b999f01f7eaec8d2e21721462e527b6
西川輝馬 <staygoldteruma@MacBookPro.local>
2021-09-15 19:40:07 +0900

    UserモデルとShopモデルのルーティングを変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,15 @@
 Rails.application.routes.draw do
-  devise_for :users
   root "fee_guides#new"
-  devise_for :shops
+  devise_for :users, controllers: {
+    sessions: "users/sessions",
+    passwords: "users/passwords",
+    registrations: "users/registrations"
+  }
+  devise_for :shops, controllers: {
+    sessions: "shops/sessions",
+    passwords: "shops/passwords",
+    registrations: "shops/registrations"
+  }
   resources :shops, only: [:index, :show]
   resources :fee_guides
   resources :main_plans
