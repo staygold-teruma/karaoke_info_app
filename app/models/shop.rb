fcdd64edd905a35b07f7fb458e493c0fb3945cf4
西川輝馬 <staygoldteruma@MacBookPro.local>
2021-09-15 19:57:04 +0900

    Usersコントローラを作成

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -4,7 +4,7 @@ class Shop < ApplicationRecord
   has_many :topics, dependent: :destroy
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable,  # :registerable,
+  devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   mount_uploader :image, ImageUploader
 
