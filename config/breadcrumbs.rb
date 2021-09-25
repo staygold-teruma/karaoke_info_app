0e13ef81d2239d8f4fec3a02c8bddce6c2126e68
西川輝馬 <staygoldteruma@MacBookPro.local>
2021-09-25 22:53:41 +0900

    パンくずリストをユーザー用のビューに実装

--- config/breadcrumbs.rb
+++ config/breadcrumbs.rb
@@ -1,15 +1,46 @@
 crumb :root do
-  link "Home", root_path
+  link "HOME", root_path
 end
 
-# crumb :projects do
-#   link "Projects", projects_path
-# end
+crumb :topic_index do
+  link "トピックス一覧", users_topics_path
+  parent :root
+end
 
-# crumb :project do |project|
-#   link project.name, project_path(project)
-#   parent :projects
-# end
+crumb :topic_show do
+  link "トピックス詳細", users_topics_path
+  parent :topic_index
+end
+
+crumb :coupon do
+  link "クーポン", static_pages_coupon_path
+  parent :root
+end
+
+crumb :alcohol do
+  link "飲み放題", static_pages_alcohol_plan_path
+  parent :root
+end
+
+crumb :user_sign_in do
+  link "ログイン", new_user_session_path
+  parent :root
+end
+
+crumb :user_sign_up do
+  link "ユーザー登録", new_user_registration_path
+  parent :root
+end
+
+crumb :user_show do |user|
+  link "マイページ", user_path(user)
+  parent :root
+end
+
+crumb :user_edit do |user|
+  link "ユーザー編集"
+  parent :user_show, user
+end
 
 # crumb :project_issues do |project|
 #   link "Issues", project_issues_path(project)
