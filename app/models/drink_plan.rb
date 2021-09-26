cb19f2bbe769be0d48e4acc99038358b541ad6a2
西川輝馬 <staygoldteruma@MacBookPro.local>
2021-09-26 11:22:50 +0900

    drink_plan.rbに追加したカラムのバリデーションとenum定義を追加

--- app/models/drink_plan.rb
+++ app/models/drink_plan.rb
@@ -3,11 +3,16 @@ class DrinkPlan < ApplicationRecord
   with_options presence: true do
     validates :name
     validates :time_unit
+    validates :base_time
     with_options numericality: { only_integer: true, greater_than_or_equal_to: 0 } do
       validates :adult_fee
       validates :student_fee
       validates :senior_fee
       validates :child_fee
+      validates :extension_adult_fee
+      validates :extension_student_fee
+      validates :extension_senior_fee
+      validates :extension_child_fee
     end
   end
   validates :note, length: { maximum: 250 }
@@ -15,5 +20,11 @@ class DrinkPlan < ApplicationRecord
   enum time_unit: {
     half_hour: 0,
     free_time: 1
+  }
+  enum base_time: {
+    half_hour: 0,
+    one_hour: 1,
+    one_half_hour: 2,
+    free_time: 3
   }
 end
