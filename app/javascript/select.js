e3c73914bdb01f9595b55dead75c369859752ed8
西川輝馬 <staygoldteruma@MacBookPro.local>
2021-10-05 18:36:46 +0900

    サンプル店舗1のTopicデータをtopic.csvに格納

--- app/javascript/select.js
+++ app/javascript/select.js
@@ -1,20 +1,20 @@
 document.addEventListener('turbolinks:load', function () {
   $(function () {
-    $('#fee_guide_usage_time').on('change', function () {
-      $('#fee_guide_drink_plan option').attr('selected', false);
-      $('#fee_guide_drink_plan').val('one_hour');
+    $('#form_fee_guide_usage_time').on('change', function () {
+      $('#form_fee_guide_drink_plan option').attr('selected', false);
+      $('#form_fee_guide_drink_plan').val('one_hour');
       let selected_time = $(this).val();
       if (selected_time == 'half_hour' || selected_time == 'one_hour') {
-        $("#fee_guide_drink_plan option[value='lite_plan']").hide();
-        $("#fee_guide_drink_plan option[value='variety_plan']").hide();
-        $("#fee_guide_drink_plan option[value='deluxe_plan']").hide();
+        $("#form_fee_guide_drink_plan option[value='lite_plan']").hide();
+        $("#form_fee_guide_drink_plan option[value='variety_plan']").hide();
+        $("#form_fee_guide_drink_plan option[value='deluxe_plan']").hide();
       } else {
-        if ($("#fee_guide_drink_plan option[value='lite_plan']").css('display') == 'block') {
+        if ($("#form_fee_guide_drink_plan option[value='lite_plan']").css('display') == 'block') {
           return;
         } else {
-          $("#fee_guide_drink_plan option[value='lite_plan']").show();
-          $("#fee_guide_drink_plan option[value='variety_plan']").show();
-          $("#fee_guide_drink_plan option[value='deluxe_plan']").show();
+          $("#form_fee_guide_drink_plan option[value='lite_plan']").show();
+          $("#form_fee_guide_drink_plan option[value='variety_plan']").show();
+          $("#form_fee_guide_drink_plan option[value='deluxe_plan']").show();
         }
       }
     });
