bb23fbc9b0504b553989e727c357772b295822b0
西川輝馬 <staygoldteruma@MacBookPro.local>
2021-10-08 17:21:53 +0900

    料金案内結果のスマホサイズのデザインのずれを修正

--- app/javascript/select.js
+++ app/javascript/select.js
@@ -2,7 +2,7 @@ document.addEventListener('turbolinks:load', function () {
   $(function () {
     $('#form_fee_guide_usage_time').on('change', function () {
       $('#form_fee_guide_drink_plan option').attr('selected', false);
-      $('#form_fee_guide_drink_plan').val('one_hour');
+      $('#form_fee_guide_drink_plan').val('one_drink');
       let selected_time = $(this).val();
       if (selected_time == 'half_hour' || selected_time == 'one_hour') {
         $("#form_fee_guide_drink_plan option[value='lite_plan']").hide();
