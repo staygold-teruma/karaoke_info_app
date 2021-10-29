f45551f9b6529082054edca24482080a641daeef
西川輝馬 <staygoldteruma@MacBookPro.local>
2021-10-30 05:26:52 +0900

    複数店舗の利用に対応できるようfee_table.jsの記述を修正

--- app/javascript/fee_table.js
+++ app/javascript/fee_table.js
@@ -1,16 +1,16 @@
 document.addEventListener('turbolinks:load', function () {
   $(function () {
-    $('#table').hide();
     $('.fee_table1').show();
+  });
+
+  $(function () {
     $('#fee_table_select').on('change', function () {
-      let selected_shop = $('#fee_table_select').val();
-      if (selected_shop == 'ikuta') {
-        $('.fee_table2').hide();
-        $('.fee_table1').show();
-      } else {
-        $('.fee_table1').hide();
-        $('.fee_table2').show();
-      }
+      let prefix = 'fee_table';
+      let select_num = $('#fee_table_select').val();
+      let target = $('.' + prefix + select_num);
+      $('.fee_table1').hide();
+      $('#fee_table').hide();
+      target.show();
     });
   });
 });
