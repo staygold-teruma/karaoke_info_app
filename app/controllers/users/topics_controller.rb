3409098cf8154459a8eabcc2ea1ab9fdd3407c8d
西川輝馬 <staygoldteruma@MacBookPro.local>
2021-11-02 05:49:08 +0900

    料金表ページの細かいデザインを調整

--- app/controllers/users/topics_controller.rb
+++ app/controllers/users/topics_controller.rb
@@ -1,6 +1,6 @@
 class Users::TopicsController < Users::ApplicationController
   before_action :set_users_topic, only: %i[show]
-  PER_PAGE = 6
+  PER_PAGE = 3
 
   def index
     @users_topics = Topic.page(params[:page]).per(PER_PAGE).order(created_at: :desc)
