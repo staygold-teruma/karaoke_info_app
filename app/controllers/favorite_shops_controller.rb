ac2b14ba2d98269d25445d9148100e5e00c8244e
西川輝馬 <staygoldteruma@MacBookPro.local>
2022-02-12 17:44:46 +0900

    favorite_shopsコントローラーにcreate/destroyアクションを記述

--- app/controllers/favorite_shops_controller.rb
+++ app/controllers/favorite_shops_controller.rb
@@ -1,2 +1,11 @@
 class FavoriteShopsController < ApplicationController
+  def create
+    current_user.favorite_shops.create!(shop_id: params[:shop_id])
+    redirect_back(fallback_location: root_path)
+  end
+
+  def destroy
+    current_user.favorite_shops.find_by(shop_id: params[:shop_id]).destroy!
+    redirect_back(fallback_location: root_path)
+  end
 end
