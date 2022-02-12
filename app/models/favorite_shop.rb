23f135de9dcbcddf23ca6038bd810f0596b5eb44
西川輝馬 <staygoldteruma@MacBookPro.local>
2022-02-12 17:24:46 +0900

    favorite_shopsモデルにuser_idとshop_idの組み合わせが一意となるようバリデーションを記述

--- app/models/favorite_shop.rb
+++ app/models/favorite_shop.rb
@@ -1,4 +1,8 @@
 class FavoriteShop < ApplicationRecord
   belongs_to :user, dependent: :destroy
   belongs_to :shop, dependent: :destroy
+  validates :user_id, uniqueness: {
+    scope: :shop_id,
+    message: "は同じ店舗をお気に入り登録できません"
+  }
 end
